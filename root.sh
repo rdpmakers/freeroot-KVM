ab2f792466a728a426d12ee0ab7beea02b774030
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-16 15:29:53 +0700

    Istg i am a stupid

--- root.sh
+++ root.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ROOTFS_DIR=$(pwd)
 if [ ! -e $ROOTFS_DIR/main.sh ]; then
-  curl -L -o root.sh https://raw.githubusercontent.com/rdpmakers/freeroot-KVM/refs/heads/main/main.sh
+  curl -L -o main.sh https://raw.githubusercontent.com/rdpmakers/freeroot-KVM/refs/heads/main/main.sh
 fi
 sh main.sh
