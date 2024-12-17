5a67a7c9f137f653bae567f9a91ed79b5c31eab4
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-17 14:25:36 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 sshpass -p "asdfqwer" ssh -o StrictHostKeyChecking=no -p2222 ubuntu@localhost "wget https://raw.githubusercontent.com/rdpmakers/freeroot-KVM/refs/heads/main/vmssh-setup.sh && sh vmssh-setup.sh"
-sshpass -p "asdfqwer" scp -o StrictHostKeyChecking=no -r -p2222 ubuntu@localhost:/home/ubuntu/ssh.key ./ssh.key
+sshpass -p "asdfqwer" sftp -oBatchMode=no -P 2222 ubuntu@localhost:/home/ubuntu/ssh.key ./ssh.key
+
