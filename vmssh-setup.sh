94ae34069c1bfe5a847897c3ff5fcaaf16c20288
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-18 18:59:17 +0700

    Update vmssh-setup.sh

--- vmssh-setup.sh
+++ vmssh-setup.sh
@@ -2,5 +2,5 @@ sudo sed -i -E 's/^#?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_conf
 sudo sed -i -E 's/^#?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 ssh-keygen -b 2048 -t rsa -N "" -f ssh.key
-sudo cat ssh.key.pub >> /root/.ssh/authorized_keys
+sudo sh -c "cat ssh.key.pub >> /root/.ssh/authorized_keys"
 sudo systemctl reload sshd
