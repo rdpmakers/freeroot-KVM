55cceaef260389337b88f54800a94e8813fef9fd
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-19 20:57:17 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ fi
 if [ ! -e $ROOTFS_DIR/.installed ]; then
   echo "#######################################################################################"
   echo "#"
-  echo "#                                      Foxytoux INSTALLER[modified by rdpmakers]"
+  echo "#                          Foxytoux INSTALLER[modified by rdpmakers]"
   echo "#"
   echo "#                           Copyright (C) 2024, RecodeStudios.Cloud"
   echo "#"
