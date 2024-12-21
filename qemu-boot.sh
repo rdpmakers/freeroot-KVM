4db0f23eeb8aca6a60bc96153e85c231d4233c2d
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-21 20:28:30 +0700

    Update qemu-boot.sh

--- qemu-boot.sh
+++ qemu-boot.sh
@@ -2,4 +2,4 @@
 #WARNING!
 #DON'T DELETE THIS FILE
 cd /root
-qemu-system-x86_64 -drive file=/root/ubuntu-22.qcow2,format=qcow2 -drive file=/root/user-data.img,format=raw -device virtio-net-pci,netdev=n0 -netdev user,id=n0,hostfwd=tcp::2222-:22,hostfwd=udp::2222-:22 -smp $(nproc) -m $(awk '/MemAvailable/ {print int($2/1024/1024)"G"}' /proc/meminfo) -accel tcg -cpu qemu64 -nographic | tee KVM.txt
+qemu-system-x86_64 -drive file=/root/ubuntu-22.qcow2,format=qcow2 -drive file=/root/user-data.img,format=raw -device virtio-net-pci,netdev=n0 -netdev user,id=n0,hostfwd=tcp::2222-:22,hostfwd=udp::2222-:22 -smp $(nproc) -m $(awk '/MemAvailable/ {print int($2/1024/1024)"G"}' /proc/meminfo) -accel tcg -cpu qemu64 -nographic | tee /opt/KVM.txt
