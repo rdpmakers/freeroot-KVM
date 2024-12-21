da90d568bbaead6adbbfe757e981f92dd82a4ada
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-21 20:47:58 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -79,7 +79,7 @@ if [ -e $ROOTFS_DIR/root/ubuntu-22.qcow2 ]; then
     $ROOTFS_DIR/usr/local/bin/proot \
     --rootfs="${ROOTFS_DIR}" \
     -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf \
-    /bin/bash -c 'sshpass -p "asdfqwer" ssh -o StrictHostKeyChecking=no -p2222 ubuntu@localhost'
+    /bin/bash /opt/ssh2qemu.sh
 else
     # If not installed, start the installation and QEMU.
     $ROOTFS_DIR/usr/local/bin/proot \
