7ba5cf8bc474d7081e8491afa4a6e25fbd9c1a6c
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2024-12-25 11:26:40 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ROOTFS_DIR=$(pwd)
 if [ ! -e $ROOTFS_DIR/main.sh ]; then
-  curl -L -o main.sh https://raw.githubusercontent.com/rdpmakers/freeroot-KVM/refs/heads/main/main.sh
+  wget -O main.sh https://raw.githubusercontent.com/rdpmakers/freeroot-KVM/refs/heads/main/main.sh
 fi
 sh main.sh
