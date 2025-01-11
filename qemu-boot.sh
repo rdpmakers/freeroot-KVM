5e3b22d99351ea3282136d4ed1458196e0e58372
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2025-01-12 00:49:34 +0700

    Update qemu-boot.sh

--- qemu-boot.sh
+++ qemu-boot.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #WARNING!
 #DON'T DELETE THIS FILE
+echo "YOU WILL BOOT INTO UBUNTU IN 2 SECONDS"
+echo "USERNAME: ubuntu"
+echo "PASSWORD: asdfqwer"
+sleep 2
 cd /root
 qemu-system-x86_64 -drive file=/root/ubuntu-22.qcow2,format=qcow2 -drive file=/root/user-data.img,format=raw -device virtio-net-pci,netdev=n0 -netdev user,id=n0,hostfwd=tcp::2222-:22,hostfwd=udp::2222-:22 -smp $(nproc) -m $(awk '/MemFree/ {print int($2/1024/1024)"G"}' /proc/meminfo) -accel tcg -cpu qemu64 -nographic | tee /opt/KVM.log
