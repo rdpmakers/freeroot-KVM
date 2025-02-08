ab69dcdb22ba793b2870bf87dc12aafd8a952aed
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2025-02-08 13:37:14 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -3,4 +3,5 @@ ROOTFS_DIR=$(pwd)
 if [ ! -e $ROOTFS_DIR/main.sh ]; then
   wget -O main.sh https://raw.githubusercontent.com/rdpmakers/freeroot-KVM/refs/heads/main/main.sh
 fi
-sh main.sh
+chmod +x main.sh
+./main.sh
