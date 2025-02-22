4e2039d6f284d348ef14830a09595e45959a5741
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2025-02-22 17:35:41 +0700

    Spill the tomato ketchup to the machine

--- qemu-boot.sh
+++ qemu-boot.sh
@@ -2,10 +2,12 @@
 #WARNING!
 #DON'T DELETE THIS FILE
 clear
-echo "YOU WILL BOOT INTO UBUNTU IN 5 SECONDS"
+echo "YOU WILL BOOT INTO UBUNTU IN 5 SECONDS AND WILL AUTOMATICALLY"
+echo "LOGGED IN AS ROOT AFTER SOME MINUTES"
+echo "IF IT TAKES SO LONG USE THIS USER INSTEAD"
 echo "USERNAME: user"
 echo "PASSWORD: U23rP422w0rd"
-echo "MAKE SURE TO SHUTDOWN USING sudo poweroff COMMAND OR YOUR VM HAS POTENTIAL TO BREAK"
+echo 'MAKE SURE TO SHUTDOWN USING "sudo poweroff" COMMAND OR YOUR VM HAS POTENTIAL TO BREAK'
 sleep 5
 cd /root
-qemu-system-x86_64 -drive file=/root/ubuntu-22.qcow2,format=qcow2 -drive file=/root/user-data.img,format=raw -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -smp $(nproc) -m $(awk '/MemFree/ {print int($2/1024/1024)"G"}' /proc/meminfo) -accel tcg -cpu kvm64 -virtfs local,path=/qemu-share,mount_tag=shared,security_model=none,id=shared -nographic | tee /opt/KVM.log
+qemu-system-x86_64 -drive file=/root/ubuntu-22.qcow2,format=qcow2 -drive file=/root/user-data.img,format=raw -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -smp $(nproc) -m $(awk '/MemFree/ {print int($2/1024/1024)"G"}' /proc/meminfo) -accel tcg -cpu kvm64,+avx -virtfs local,path=/qemu-share,mount_tag=shared,security_model=none,id=shared -nographic | tee /opt/KVM.log
